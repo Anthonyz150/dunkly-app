5d9f26252457e540781ead9765ac23c0596cf3d3
Anthonyz150 <anthony.didier.pro@gmail.com>
2026-02-10 20:18:27 +0100

    App

--- electron.js
+++ electron.js
@@ -5,12 +5,14 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
+    title: "Dunkly", // 👈 AJOUTE CETTE LIGNE
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false, 
     },
   });
 
+  win.setMenu(null);
   win.loadURL('https://dunkly-app.vercel.app'); 
 }
 
