b870be4b3e3d606204540f8588621a84c7af5e5d
Anthonyz150 <anthony.didier.pro@gmail.com>
2026-02-10 22:12:51 +0100

    App

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,12 @@
-import { createBrowserClient } from "@supabase/ssr";
+import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabaseUrl = 'https://jpudcvzrjqzdlhitxnbw.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpwdWRjdnpyanF6ZGxoaXR4bmJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNDQ1OTksImV4cCI6MjA4NTcyMDU5OX0.bXHHsEogjmMeI-zDvxvmiH1KnF_JinJ_NR94Or9dS_E';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    // Il faut que ce nom soit le même dans les deux projets
+    storageKey: 'dunkly-auth-token', 
+    storage: typeof window !== 'undefined' ? window.localStorage : undefined,
+  },
+});
